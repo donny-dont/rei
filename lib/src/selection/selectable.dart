b37c211b0ae2e8afada7118f14eef9d40b5fa88b
Don <don.j.olmstead@gmail.com>
2015-11-18 20:46:22 -0800

    Adding selectable interface and helpers

---
+++ lib/src/selection/selectable.dart
@@ -0,0 +1,155 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+//---------------------------------------------------------------------
+// Standard libraries
+//---------------------------------------------------------------------
+
+import 'dart:html' as html;
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// An interface for implementing selection through a gamepad.
+///
+/// A gamepad provides movement in five directions. Four of the directions,
+/// [up], [down], [left], [right], are handled through the d-pad. The fifth,
+/// [back], is the right face button.
+abstract class Selectable {
+  //---------------------------------------------------------------------
+  // Class variables
+  //---------------------------------------------------------------------
+
+  /// Specifies that an element is not selectable.
+  ///
+  /// By default any items in the [Selectable] group can be selected. This
+  /// This attribute should be added to any element that cannot be selected.
+  static const String notSelectableAttribute = 'data-not-selectable';
+  /// The event fired when the selected element is changed.
+  static const String selectionChangedEvent = 'selectionchange';
+
+  //---------------------------------------------------------------------
+  // Properties
+  //---------------------------------------------------------------------
+
+  /// The currently selected element.
+  html.Element get selectedElement;
+  /// A list of elements that can be selected from.
+  List<html.Element> get selectableElements;
+  /// Whether the selectable area allows wrapping from the first element.
+  ///
+  /// If wrapStart is `true` then it is possible to move from the first
+  /// element within [selectableElements] to the last element within it.
+  bool get wrapStart;
+  /// Whether the selectable area allows wrapping from the last element.
+  ///
+  /// If wrapEnd is `true` then it is possible to move from the lase element
+  /// within [selectableElements] to the first element within it.
+  bool get wrapEnd;
+  /// Whether the selectable area responds to the back command.
+  bool get canMoveBack;
+
+  //---------------------------------------------------------------------
+  // Public methods
+  //---------------------------------------------------------------------
+
+  /// Enters the selectable area.
+  ///
+  /// When entering a [Selectable] area the [previous] element that was
+  /// was selected should be passed into the method. This gives the
+  /// implementation the ability to make a decision based on the previously
+  /// selected element.
+  void select([html.Element previous = null]);
+
+  /// Exits the selectable area.
+  ///
+  /// The [back] method is used for when the user needs to exit a selectable
+  /// area. This is useful for when the selection area needs to be constrained
+  /// within a component.
+  void back();
+
+  /// Moves the selection up.
+  void up();
+
+  /// Moves the selection down.
+  void down();
+
+  /// Moves the selection to the left.
+  void left();
+
+  /// Moves the selection to the right.
+  void right();
+
+  /// Resets the selection.
+  void resetSelection();
+
+  //---------------------------------------------------------------------
+  // Class methods
+  //---------------------------------------------------------------------
+
+  /// Determines if the [element] can be selected.
+  ///
+  /// This method checks to see if the [element] has attached the
+  /// [notSelectableAttribute]. The check does not account for the element not
+  /// being visible within the DOM. This is because the style would have to be
+  /// computed which can be a time consuming operation.
+  static bool canSelect(html.Element element) {
+    return !element.attributes.containsKey(notSelectableAttribute) &&
+        element is! html.StyleElement &&
+        element is! html.TemplateElement;
+  }
+
+  /// Finds the first [Html.Element] in elements that can be selected.
+  ///
+  /// Uses the [canSelect] method to determine what is selectable.
+  static html.Element findFirstSelectable(List<html.Element> elements) {
+    return elements.firstWhere((element) => canSelect(element),
+        orElse: () => null);
+  }
+
+  /// Finds the last [Html.Element] in elements that can be selected.
+  ///
+  /// Uses the [canSelect] method to determine what is selectable.
+  static html.Element findLastSelectable(List<html.Element> elements) {
+    return elements.lastWhere((element) => canSelect(element),
+        orElse: () => null);
+  }
+
+  /// Finds the parent of a selected [element].
+  ///
+  /// This method is used to search both the light DOM and the shadow DOM for
+  /// the parent.
+  static Selectable findParentSelectable(html.Element element) {
+    // See if the element is in the light DOM
+    var search;
+
+    search = element.parent;
+
+    while (search != null) {
+      if (search is Selectable) {
+        return search;
+      }
+
+      search = search.parent;
+    }
+
+    // See if the element is in the shadow DOM
+    search = element.parentNode;
+
+    while (search != null) {
+      if (search is html.ShadowRoot) {
+        // The host should always be selectable otherwise something has gone
+        // horribly wrong
+        assert(search.host is Selectable);
+
+        return search.host as Selectable;
+      }
+
+      search = search.parentNode;
+    }
+
+    // Not selectable!
+    return null;
+  }
+}
