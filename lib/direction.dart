8bc0237eb9727c4e33de8a988d4d7078ad599ada
Don <don.j.olmstead@gmail.com>
2015-11-18 17:12:32 -0800

    Adding the direction enumeration

---
+++ lib/direction.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+/// Contains the [Direction] enumeration.
+library rei.direction;
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Defines a direction for an element to flow in.
+enum Direction {
+  /// The element flows horizontally, left and right.
+  horizontal,
+  /// The element flows vertically, up and down.
+  vertical
+}
