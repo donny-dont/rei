53f6808fe3f763cfff9914bc6cfe51a46f92e255
Don <don.j.olmstead@gmail.com>
2015-11-18 18:41:06 -0800

    Adding test for direction

---
+++ test/direction_test.dart
@@ -0,0 +1,39 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+library rei.test.direction_serializer.direction_serializer;
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:rei/direction.dart';
+import 'package:test/test.dart';
+
+//---------------------------------------------------------------------
+// Tests
+//---------------------------------------------------------------------
+
+void _deserialize() {
+  expect(deserializeDirection('horizontal'), Direction.horizontal);
+  expect(deserializeDirection('vertical'), Direction.vertical);
+
+  // Check defaults
+  expect(deserializeDirection('0000'), Direction.horizontal);
+}
+
+void _serialize() {
+  expect(serializeDirection(Direction.horizontal), 'horizontal');
+  expect(serializeDirection(Direction.vertical), 'vertical');
+}
+
+//---------------------------------------------------------------------
+// Entry point
+//---------------------------------------------------------------------
+
+void main() {
+  group('DirectionSerializer', () {
+    test('deserializeDirection', _deserialize);
+    test('serializeDirection', _serialize);
+  });
+}
