e36b6c66930e14457363eb9d4a9608b0f96b7bcc
Don <don.j.olmstead@gmail.com>
2015-11-20 15:46:55 -0800

    Adding the transformable library

---
+++ lib/transformable.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+/// Contains behaviors for transformations within Polymer elements.
+library rei.transformable;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/transformable/transformable.dart';
+export 'src/transformable/transformable_element.dart';
+export 'src/transformable/transformable_manager.dart';
