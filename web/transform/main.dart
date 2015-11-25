273d8299138fbd12feb9f53b78ba6c29c408413e
Don <don.j.olmstead@gmail.com>
2015-11-24 18:10:03 -0800

    Adding animations to example transform

--- web/transform/main.dart
+++ web/transform/main.dart
@@ -16,9 +16,14 @@ import 'dart:html' as html;
 
 import 'package:polymer/polymer.dart';
 
+import 'package:rei/components/easing_animation.dart';
+import 'package:rei/components/selection_group.dart';
 import 'package:rei/components/transform.dart';
 import 'package:rei/components/transform_group.dart';
+import 'package:rei/animation.dart';
+import 'package:rei/selection_visualizer.dart';
 import 'package:rei/transformable.dart';
+import 'package:rei/src/bounds.dart';
 
 //---------------------------------------------------------------------
 // Library contents
@@ -27,15 +32,38 @@ import 'package:rei/transformable.dart';
 /// Application entry-point.
 /// [TransformGroup]
 /// [Transform]
+/// [EasingAnimation]
 Future<Null> main() async {
   await initPolymer();
 
+  html.document.body.style.opacity = '1';
+
+  // Create the transformable manager and update the current positioning
   var transformableManager = new TransformableManager();
+  transformableManager.update();
+
+  // Get the root selection
+  var root = html.querySelector(SelectionGroup.customTagName) as SelectionGroup;
+
+  // Compute the bounds tree
+  computeBoundsTree(root);
+
+  // Create the visualizer
+  var visualizer = new SelectionVisualizer('canvas');
+  visualizer.root = root;
+
+  // Create the animation manager
+  var animationManager = new AnimationManager();
+
+  var lastTime = await html.window.animationFrame;
 
   while (true) {
     var time = await html.window.animationFrame;
 
-    //print(time);
+    animationManager.update(time - lastTime);
     transformableManager.update();
+    visualizer.draw();
+
+    lastTime = time;
   }
 }
