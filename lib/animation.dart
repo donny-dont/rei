c791d661864f0d6b26b520eeb3262f3312d37fc2
Don <don.j.olmstead@gmail.com>
2015-11-24 18:08:54 -0800

    Adding animation library

---
+++ lib/animation.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+/// Contains behaviors for animations using Polymer elements.
+library rei.animation;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/animation/animation.dart';
+export 'src/animation/animation_element.dart';
+export 'src/animation/animation_manager.dart';
+export 'src/animation/animation_value.dart';
+export 'src/animation/bezier_curve.dart';
+export 'src/animation/bezier_curve_animation.dart';
+export 'src/animation/css_property.dart';
+export 'src/animation/easing_function_curves.dart';
+export 'src/animation/interval_animation.dart';
