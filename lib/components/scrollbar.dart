76a10384e8a6cb6111ef685adfaf17fbb09c42d3
Don <don.j.olmstead@gmail.com>
2015-11-30 18:19:23 -0800

    Adding scroll bar implementation

---
+++ lib/components/scrollbar.dart
@@ -0,0 +1,136 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+/// Contains the [Scrollbar] class.
+@HtmlImport('scrollbar.html')
+library rei.components.scrollbar;
+
+//---------------------------------------------------------------------
+// Standard libraries
+//---------------------------------------------------------------------
+
+import 'dart:html' as html;
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart' show HtmlImport;
+
+import 'package:rei/animatable.dart';
+import 'package:rei/direction.dart';
+import 'package:rei/transformable.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Tag name for the class.
+const String _tagName = 'rei-scrollbar';
+
+@PolymerRegister(_tagName)
+class Scrollbar extends PolymerElement
+                   with Animatable,
+                        TransformableElement,
+                        Transformable,
+                        PolymerSerialize {
+  //---------------------------------------------------------------------
+  // Class variables
+  //---------------------------------------------------------------------
+
+  /// The name of the tag.
+  static const String customTagName = _tagName;
+
+  //---------------------------------------------------------------------
+  // Construction
+  //---------------------------------------------------------------------
+
+  /// Creates an instance of the [Scrollbar] class.
+  factory Scrollbar() =>
+      new html.Element.tag(customTagName) as Scrollbar;
+
+  /// Create an instance of the [Scrollbar] class.
+  ///
+  /// This constructor should not be called directly. Instead use the
+  /// default constructor.
+  Scrollbar.created() : super.created();
+
+  //---------------------------------------------------------------------
+  // Attributes
+  //---------------------------------------------------------------------
+
+  @Property(reflectToAttribute: true)
+  Direction direction = Direction.horizontal;
+
+  //---------------------------------------------------------------------
+  // PolymerElement
+  //---------------------------------------------------------------------
+
+  void ready() {
+    readyTransformableElement();
+  }
+
+  void attached() {
+    attachedTransformableElement();
+  }
+
+  void detached() {
+    detachedTransformableElement();
+  }
+
+  //---------------------------------------------------------------------
+  // Animatable
+  //---------------------------------------------------------------------
+
+  @override
+  html.Element get animatableElement => this;
+
+  //---------------------------------------------------------------------
+  // Transformable
+  //---------------------------------------------------------------------
+
+  @override
+  num x = 0.0;
+
+  @override
+  num y = 0.0;
+
+  @override
+  num get scaleX => 1.0;
+  set scaleX(num value) {
+    throw new UnsupportedError('cannot set scaleX');
+  }
+
+  @override
+  num get scaleY => 1.0;
+  set scaleY(num value) {
+    throw new UnsupportedError('cannot set scaleY');
+  }
+
+  @override
+  bool get propagateTransformChange => false;
+
+  //---------------------------------------------------------------------
+  // TransformableElement
+  //---------------------------------------------------------------------
+
+  @override
+  html.Element get transformableElement => $['thumb'];
+
+  //---------------------------------------------------------------------
+  // PolymerSerialize
+  //---------------------------------------------------------------------
+
+  @override
+  dynamic deserialize(String value, Type type) =>
+      (type == Direction)
+          ? deserializeDirection(value)
+          : super.deserialize(value, type);
+
+  @override
+  String serialize(Object value) =>
+      (value is Direction)
+          ? serializeDirection(value)
+          : super.serialize(value);
+}
