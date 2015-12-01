0f28f6caa1c03e0ae91c28a06c1bffa4696cda89
Don <don.j.olmstead@gmail.com>
2015-11-30 18:19:35 -0800

    Adding scrollable group implementation

---
+++ lib/components/scrollable_group.dart
@@ -0,0 +1,152 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+/// Contains the [SelectionGroup] class.
+@HtmlImport('scrollable_group.html')
+library rei.components.scrollable_group;
+
+//---------------------------------------------------------------------
+// Standard libraries
+//---------------------------------------------------------------------
+
+import 'dart:html' as html;
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart' show HtmlImport;
+
+import 'package:rei/animatable.dart';
+import 'package:rei/animation_target.dart';
+import 'package:rei/direction.dart';
+import 'package:rei/selectable.dart';
+import 'package:rei/transformable.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Tag name for the class.
+const String _tagName = 'rei-scrollable-group';
+
+@PolymerRegister(_tagName)
+class ScrollableGroup extends PolymerElement
+                         with Animatable,
+                              LinearSelectable,
+                              TransformableElement,
+                              Transformable,
+                              PolymerSerialize {
+  //---------------------------------------------------------------------
+  // Class variables
+  //---------------------------------------------------------------------
+
+  /// The name of the tag.
+  static const String customTagName = _tagName;
+
+  //---------------------------------------------------------------------
+  // Attributes
+  //---------------------------------------------------------------------
+
+  @override
+  @Property(reflectToAttribute: true)
+  Direction direction = Direction.horizontal;
+  @override
+  @Property(reflectToAttribute: true)
+  bool canMoveBack = true;
+  @override
+  @Property(reflectToAttribute: true)
+  bool wrapStart = false;
+  @override
+  @Property(reflectToAttribute: true)
+  bool wrapEnd = false;
+  @override
+  @Property(reflectToAttribute: true)
+  num scaleX = 1.0;
+  @override
+  @Property(reflectToAttribute: true)
+  num scaleY = 1.0;
+  @override
+  @Property(reflectToAttribute: true)
+  num x = 0.0;
+  @override
+  @Property(reflectToAttribute: true)
+  num y = 0.0;
+  /// Whether the scrollbar should be visible.
+  @Property(reflectToAttribute: true)
+  bool scrollbar = false;
+  @property
+  AnimationTarget get animationTarget =>
+      (direction == Direction.horizontal)
+          ? AnimationTarget.translateX
+          : AnimationTarget.translateY;
+
+  //---------------------------------------------------------------------
+  // Construction
+  //---------------------------------------------------------------------
+
+  /// Creates an instance of the [ScrollableGroup] class.
+  factory ScrollableGroup() =>
+      new html.Element.tag(customTagName) as ScrollableGroup;
+
+  /// Create an instance of the [ScrollableGroup] class.
+  ///
+  /// This constructor should not be called directly. Instead use the
+  /// default constructor.
+  ScrollableGroup.created() : super.created();
+
+  //---------------------------------------------------------------------
+  // Animatable
+  //---------------------------------------------------------------------
+
+  @override
+  html.Element get animatableElement => this;
+
+  //---------------------------------------------------------------------
+  // LinearSelectable
+  //---------------------------------------------------------------------
+
+  @override
+  List<html.Element> get selectableElements =>
+      (Polymer.dom(this) as PolymerDom).children;
+
+  //---------------------------------------------------------------------
+  // TransformableElement
+  //---------------------------------------------------------------------
+
+  @override
+  html.Element get transformableElement => $['content-wrapper'];
+
+  //---------------------------------------------------------------------
+  // PolymerElement
+  //---------------------------------------------------------------------
+
+  void ready() {
+    readyTransformableElement();
+  }
+
+  void attached() {
+    attachedTransformableElement();
+  }
+
+  void detached() {
+    detachedTransformableElement();
+  }
+
+  //---------------------------------------------------------------------
+  // PolymerSerialize
+  //---------------------------------------------------------------------
+
+  @override
+  dynamic deserialize(String value, Type type) =>
+      (type == Direction)
+          ? deserializeDirection(value)
+          : super.deserialize(value, type);
+
+  @override
+  String serialize(Object value) =>
+      (value is Direction)
+          ? serializeDirection(value)
+          : super.serialize(value);
+}
