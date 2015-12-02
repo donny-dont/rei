9098329d58be8f288ee4a42af500c3a9283332b7
Don <don.j.olmstead@gmail.com>
2015-12-01 17:06:43 -0800

    Add the application library

---
+++ lib/application.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+/// Contains behaviors for animations using Polymer elements.
+library rei.application;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/application/time.dart';
+export 'src/application/update_loop.dart';
