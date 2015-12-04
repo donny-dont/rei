ca51f6bc0d32732cccef04d25947b1f39e89aa04
Don <don.j.olmstead@gmail.com>
2015-12-03 19:50:41 -0800

    Add layout classes

---
+++ lib/components/layout.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+/// Contains styles for elements.
+@HtmlImport('layout.html')
+library rei.components.layout;
+
+import 'package:web_components/web_components.dart' show HtmlImport;
