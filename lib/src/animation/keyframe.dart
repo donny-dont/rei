1cdc075b34a8b5692e4037ea9695dc05894e8948
Don <don.j.olmstead@gmail.com>
2015-12-14 18:38:12 -0800

    Fleshing out keyframe class

---
+++ lib/src/animation/keyframe.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2015, the Rei Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+
+/// Contains the [Keyframe] mixin.
+library rei.src.animation.keyframe;
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Represents a keyframe within an animation.
+abstract class Keyframe<T> {
+  /// The offset of the start of the keyframe.
+  ///
+  /// This needs to be a value in the range \[0, 1\]. The actual duration of
+  /// the keyframe is determined by the offset which is a percentage of the
+  /// animation's duration.
+  num get offset;
+  set offset(num value);
+  /// The value of the keyframe.
+  T get value;
+}
