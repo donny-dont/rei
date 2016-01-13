d8478bbd01e79648a8dea6497361b4d174fdde16
Don <don.j.olmstead@gmail.com>
2016-01-12 19:02:33 -0800

    Adding new bezier curve animation implementation

--- lib/src/animation/bezier_curve_animation.dart
+++ lib/src/animation/bezier_curve_animation.dart
@@ -5,12 +5,20 @@
 library rei.src.animation.bezier_curve_animation;
 
 //---------------------------------------------------------------------
+// Standard libraries
+//---------------------------------------------------------------------
+
+import 'dart:html' as html;
+
+//---------------------------------------------------------------------
 // Imports
 //---------------------------------------------------------------------
 
 import 'package:rei/bezier_curve.dart';
 
 import 'animation.dart';
+import 'animation_target_value.dart';
+import 'computed_timing.dart';
 
 //---------------------------------------------------------------------
 // Library contents
@@ -56,4 +64,33 @@ abstract class BezierCurveAnimation<T> implements Animation<T> {
   double get currentTime;
   set currentTime(double value);
   double get animationTime;
+}
+
+/// An animation whose computed value is based on a single bezier curve.
+///
+/// A [BezierCurveAnimation] is the simplest animation type and functions the
+/// same as a CSS transition. It contains a [start] value and an [end] value
+/// with which the animation is computed using over the [duration].
+abstract class BezierCurveAnimation2<T> implements ComputedTiming, Animation2 {
+  //---------------------------------------------------------------------
+  // Properties
+  //---------------------------------------------------------------------
+
+  /// The bezier curve.
+  BezierCurve<T> get curve;
+  /// The starting value of the animation.
+  T get start;
+  /// The ending value of the animation.
+  T get end;
+
+  //---------------------------------------------------------------------
+  // ComputedTiming
+  //---------------------------------------------------------------------
+
+  @override
+  void onLocalTimeUpdate() {
+    var value = curve.solve(animationTime, duration, start, end) as num;
+
+    applyAnimationTargetValue(animationTarget, animatedElement, value);
+  }
 }
