d76f090b66305f281761939eae0f322e746b7f0c
Don <don.j.olmstead@gmail.com>
2016-01-19 19:19:34 -0800

    Adding a way to force the creation of the curve

--- lib/src/animation/easing_value.dart
+++ lib/src/animation/easing_value.dart
@@ -49,18 +49,38 @@ abstract class EasingValue {
   /// The easing function or bezier curve to use when computing the animation
   /// value.
   dynamic get easing;
+  set easing(dynamic value);
+
+  //---------------------------------------------------------------------
+  // Public methods
+  //---------------------------------------------------------------------
+
+  /// Creates the curve associated with the easing value.
+  ///
+  /// This is here because the observer only seems to work when the element is
+  /// already in the DOM. This seems to be needed when creating things in code
+  /// and can be used as a trigger when the value is changed.
+  ///
+  /// \TODO Verify if needed
+  void createCurve() {
+    var curveValues = (easing is EasingFunction)
+        ? getEasingCurve(easing)
+        : easing;
+
+    // \TODO actually base curve on the target
+    _curve = new BezierCurveScalar(curveValues as List<num>);
+  }
 
   //---------------------------------------------------------------------
   // Callbacks
   //---------------------------------------------------------------------
 
+  /// Callback for when the value of [easing] changes.
+  ///
+  /// This will trigger the creation of the [curve] which will be used for the
+  /// animation.
   @Observe('easing')
   void easingChanged(dynamic value) {
-    var curveValues = (value is EasingFunction)
-        ? getEasingCurve(value)
-        : value;
-
-    // \TODO actually base curve on <T>
-    _curve = new BezierCurveScalar(curveValues as List<num>);
+    createCurve();
   }
 }
